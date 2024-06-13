1c9d606fa9e1d71ad00780943e1bfbe21f0cb96b
Lorenzo Villa <lorenzo.villa@ynap.com>
2024-06-13 15:44:49 +0200

    chore: refactor booklistview

--- PimpLibrary/Views/BookListView.swift
+++ PimpLibrary/Views/BookListView.swift
@@ -10,6 +10,8 @@ struct BookListView: View {
     @State private var sortCriteria = "Title"
     @State private var isAscending = true
     @State private var isLoading = false
+    @State private var textOffset: CGFloat = -UIScreen.main.bounds.width
+    @State private var textOpacity: Double = 0.0
     @AppStorage("useGridView") private var useGridView: Bool = false
 
     init(viewModel: LibraryViewModel = LibraryViewModel(bookRepository: RealmBookRepository())) {
@@ -17,105 +19,23 @@ struct BookListView: View {
     }
 
     var sortedBooks: [Book] {
-        let sortedBooks: [Book]
-        switch sortCriteria {
-        case "Title":
-            sortedBooks = viewModel.books.sorted { isAscending ? $0.title < $1.title : $0.title > $1.title }
-        case "Author":
-            sortedBooks = viewModel.books.sorted { isAscending ? $0.author < $1.author : $0.author > $1.author }
-        case "Year":
-            sortedBooks = viewModel.books.sorted { isAscending ? $0.year < $1.year : $0.year > $1.year }
-        default:
-            sortedBooks = viewModel.books
-        }
-        return sortedBooks
+        sortBooks(books: viewModel.books, by: sortCriteria, ascending: isAscending)
     }
 
     var filteredBooks: [Book] {
-        if searchText.isEmpty {
-            return sortedBooks
-        } else {
-            return sortedBooks.filter { $0.title.lowercased().contains(searchText.lowercased()) }
-        }
+        filterBooks(books: sortedBooks, by: searchText)
     }
 
     var body: some View {
         VStack {
-            HStack {
-                Text("My Library")
-                    .font(.largeTitle)
-                    .bold()
-                Spacer()
-                Button(action: {
-                    withAnimation {
-                        isSortingVisible.toggle()
-                    }
-                }) {
-                    Image(systemName: "arrow.up.arrow.down")
-                        .imageScale(.large)
-                }
-                .padding(.trailing, 10)
-                Button(action: {
-                    withAnimation {
-                        isSearchBarVisible.toggle()
-                    }
-                }) {
-                    Image(systemName: "magnifyingglass")
-                        .imageScale(.large)
-                }
-                .padding(.trailing, 10)
-                NavigationLink(destination: AddBookView(viewModel: viewModel)) {
-                    Image(systemName: "plus")
-                        .imageScale(.large)
-                }
-                .padding(.trailing)
-            }
-            .padding([.leading, .trailing, .top])
-
-            if isSearchBarVisible {
-                SearchBarView(text: $searchText)
-                    .transition(.move(edge: .top).combined(with: .opacity))
-            }
-
-            if isSortingVisible {
-                HStack {
-                    Picker("Sort by", selection: $sortCriteria) {
-                        Text("Title").tag("Title")
-                        Text("Author").tag("Author")
-                        Text("Year").tag("Year")
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
-                    .padding(.horizontal)
-
-                    Toggle(isOn: $isAscending) {
-                        Text(isAscending ? "Ascending" : "Descending")
-                    }
-                    .padding(.horizontal)
-                }
-                .transition(.move(edge: .top).combined(with: .opacity))
-            }
-
-            if isLoading {
-                ProgressView()
-                    .progressViewStyle(CircularProgressViewStyle())
-                    .scaleEffect(1.5, anchor: .center)
-                    .padding()
-            }
-
+            headerView
+            if isSearchBarVisible { SearchBarView(text: $searchText).transition(.move(edge: .top).combined(with: .opacity)) }
+            if isSortingVisible { sortingOptionsView.transition(.move(edge: .top).combined(with: .opacity)) }
+            if isLoading { ProgressView().progressViewStyle(CircularProgressViewStyle()).scaleEffect(1.5, anchor: .center).padding() }
             if useGridView {
-                BookGridItems(
-                    filteredBooks: filteredBooks,
-                    viewModel: viewModel,
-                    refreshBooks: refreshBooks,
-                    confirmDelete: confirmDelete(at:)
-                )
+                BookGridItems(filteredBooks: filteredBooks, viewModel: viewModel, refreshBooks: refreshBooks, confirmDelete: confirmDelete(at:))
             } else {
-                BookListItems(
-                    filteredBooks: filteredBooks,
-                    viewModel: viewModel,
-                    refreshBooks: refreshBooks,
-                    confirmDelete: confirmDelete(at:)
-                )
+                BookListItems(filteredBooks: filteredBooks, viewModel: viewModel, refreshBooks: refreshBooks, confirmDelete: confirmDelete(at:))
             }
         }
         .navigationBarHidden(true)
@@ -124,16 +44,76 @@ struct BookListView: View {
                 title: Text("Delete Book"),
                 message: Text("Are you sure you want to delete \(bookToDelete?.title ?? "")?"),
                 primaryButton: .destructive(Text("Delete")) {
-                    if let book = bookToDelete {
-                        viewModel.removeBook(book: book)
-                    }
+                    if let book = bookToDelete { viewModel.removeBook(book: book) }
                 },
                 secondaryButton: .cancel()
             )
         }
+        .onAppear {
+            animateTextEntrance()
+        }
     }
 
-    func refreshBooks() {
+    private var headerView: some View {
+        HStack {
+            Text("PimpLibrary")
+                .font(.largeTitle)
+                .bold()
+                .offset(x: textOffset)
+                .opacity(textOpacity)
+            Spacer()
+            Button(action: { withAnimation { isSortingVisible.toggle() } }) {
+                Image(systemName: "arrow.up.arrow.down").imageScale(.large)
+            }
+            .padding(.trailing, 10)
+            Button(action: { withAnimation { isSearchBarVisible.toggle() } }) {
+                Image(systemName: "magnifyingglass").imageScale(.large)
+            }
+            .padding(.trailing, 10)
+            NavigationLink(destination: AddBookView(viewModel: viewModel)) {
+                Image(systemName: "plus").imageScale(.large)
+            }
+            .padding(.trailing)
+        }
+        .padding([.leading, .trailing, .top])
+    }
+
+    private var sortingOptionsView: some View {
+        HStack {
+            Picker("Sort by", selection: $sortCriteria) {
+                Text("Title").tag("Title")
+                Text("Author").tag("Author")
+                Text("Year").tag("Year")
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding(.horizontal)
+
+            Toggle(isOn: $isAscending) {
+                Text(isAscending ? "Ascending" : "Descending")
+            }
+            .padding(.horizontal)
+        }
+    }
+
+    private func sortBooks(books: [Book], by criteria: String, ascending: Bool) -> [Book] {
+        switch criteria {
+        case "Title":
+            return books.sorted { ascending ? $0.title < $1.title : $0.title > $1.title }
+        case "Author":
+            return books.sorted { ascending ? $0.author < $1.author : $0.author > $1.author }
+        case "Year":
+            return books.sorted { ascending ? $0.year < $1.year : $0.year > $1.year }
+        default:
+            return books
+        }
+    }
+
+    private func filterBooks(books: [Book], by searchText: String) -> [Book] {
+        if searchText.isEmpty { return books }
+        return books.filter { $0.title.lowercased().contains(searchText.lowercased()) }
+    }
+
+    private func refreshBooks() {
         isLoading = true
         DispatchQueue.global().async {
             let books = viewModel.bookRepository.fetchBooks()
@@ -144,13 +124,20 @@ struct BookListView: View {
         }
     }
 
-    func confirmDelete(at offsets: IndexSet) {
+    private func confirmDelete(at offsets: IndexSet) {
         for index in offsets {
             let book = filteredBooks[index]
             if let bookIndex = viewModel.books.firstIndex(where: { $0.id == book.id }) {
                 bookToDelete = viewModel.books[bookIndex]
                 showingAlert = true
             }
+        }
+    }
+
+    private func animateTextEntrance() {
+        withAnimation(Animation.easeInOut(duration: 1.0)) {
+            textOffset = 0
+            textOpacity = 1.0
         }
     }
 }
